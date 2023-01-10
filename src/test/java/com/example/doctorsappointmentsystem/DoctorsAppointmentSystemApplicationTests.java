7054937b320d36de23b763b9618d1a7e5ca0724b
hamid <74140209+hamid423@users.noreply.github.com>
2023-01-10 23:04:17 +0100

    first commit

---
+++ src/test/java/com/example/doctorsappointmentsystem/DoctorsAppointmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doctorsappointmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorsAppointmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
